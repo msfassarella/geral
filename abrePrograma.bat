5a5598f966d164016637a3680bfa49b45792ebce
msfassarella <33352657+msfassarella@users.noreply.github.com>
2019-08-26 22:41:22 -0300

    Commit inicial
    Programa usado em conjunto com executaPrograma.py e fechaPrograma.py

---
+++ abrePrograma.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Scritp para ser inicializado com o Windows
+C:\Python33\python sneckpython.py
+pause
+//#"C:\Program Files (x86)\Snek\snek.vbs"
+//Programa usado em conjunto com executaPrograma.py e fechaPrograma.py
+//Este arquivo deve ser colocado para executar junto com a inicialização do Windows.
